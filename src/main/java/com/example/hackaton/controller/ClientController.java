e901111e20e9a7374edbe68bbeb63be28066a278
Adrian Kulawiec <AKL@bec.dk>
2023-05-12 09:36:58 +0200

    global cors enabled

--- src/main/java/com/example/hackaton/controller/ClientController.java
+++ src/main/java/com/example/hackaton/controller/ClientController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import java.util.Optional;
 
+
+
+/**
+ * This class is used for sharing client information for siteKey frontendApp.
+ * Business case behind it's creation was to make frontend mock able to download
+ */
 @RestController
 @RequestMapping("/users")
 public class ClientController {
@@ -26,10 +31,21 @@ public class ClientController {
         return users.findAll();
     }
 
+    /**
+     * method created mostly for tasting purposes only, does not implement business logic
+     * @param username is the name of the user we would like to get information about
+     * @return  user or null
+     */
     @GetMapping("/{username}")
     public Client getUser(@PathVariable String username) {
         return users.findByLoginName(username).orElse(null);
     }
+
+    /**
+     * method is used for fetching question for the verification user case in ot
+     * @param username is the name of the user we would like to get information about
+     * @return question for the user
+     */
     @GetMapping("/{username}/question")
     public String getQuestion(@PathVariable String username) {
        return users.findByLoginName(username)
